481b7980f02314f81f060b778843b0883ee5edc3
Otto <otto.vanto@aalto.fi>
2025-04-29 11:50:15 +0300

    working version

--- task_template/frontend_poetry/src/components/TaskDescription.jsx
+++ task_template/frontend_poetry/src/components/TaskDescription.jsx
@@ -4,11 +4,11 @@ const TaskDescription = () => {
       <h2>🖋️ Poetry task</h2>
       <h3>📜 Rules</h3>
       <ol>
-        <li>Provide a theme for the story.</li>
-        <li>The AI then writes the first draft.</li>
+        <li>Provide a theme for the poem.</li>
+        <li>The AI then writes the first line.</li>
         <li>Then it is your turn to write.</li>
-        <li>To discuss the story, write in the Dialogue box.</li>
-        <li>When you think you are ready, click finish.</li>
+        <li>If you want to discuss with the AI, you can do that in the Dialogue box.</li>
+        <li>The game ends when the poem has 9 lines.</li>
         <li>Please then rate your experience with the AI.</li>
       </ol>
     </div>
